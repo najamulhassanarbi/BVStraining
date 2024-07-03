af905b357fe54a61471488f954685af55cc084ed
najamulhassanarbi <najam.hassan02@arbisoft.com>
2024-07-03 18:22:30 +0500

    Removed the data folder, added it to git ignore, fixed the ccounter

--- Weather Man/weatherman.py
+++ Weather Man/weatherman.py
@@ -1,10 +1,8 @@
 # weatherman.py
-
-import sys
-import os
 from file_parser import WeatherParser
 from weather_calculator import WeatherCalculator
 from weather_reports import WeatherReport
+import sys
 
 def main():
     args = sys.argv[1:]
@@ -19,27 +17,27 @@ def main():
     weather_data = parser.parse_files()
     calculator = WeatherCalculator(weather_data)
 
-    i = 0
-    while i < len(options):
-        option = options[i]
+    option_index = 0
+    while option_index < len(options):
+        option = options[option_index]
         if option == '-e':
-            year = int(options[i + 1])
+            year = int(options[option_index + 1])
             results = calculator.calculate_yearly_extremes(year)
             report = WeatherReport(results)
             report.generate_extremes_report()
-            i += 2
+            option_index += 2
         elif option == '-a':
-            year, month = map(int, options[i + 1].split('/'))
+            year, month = map(int, options[option_index + 1].split('/'))
             results = calculator.calculate_monthly_averages(year, month)
             report = WeatherReport(results)
             report.generate_averages_report()
-            i += 2
+            option_index += 2
         elif option == '-c':
             year, month = map(int, options[i + 1].split('/'))
             results = calculator.calculate_daily_temperatures(year, month)
             report = WeatherReport(results)
             report.generate_daily_temperatures_report(year, month)
-            i += 2
+            option_index += 2
         else:
             print("Invalid option:", option)
             sys.exit(1)
