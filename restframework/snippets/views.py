0faa2cffbadf84572d2dd14c6fe1c6ae2d5b3523
najamulhassanarbi <najam.hassan02@arbisoft.com>
2024-07-24 16:10:51 +0500

    completed tutorial 4 for adding authentication, permissions

--- restframework/snippets/views.py
+++ restframework/snippets/views.py
@@ -1,11 +1,14 @@
 from django.http import Http404
 from rest_framework import status
+from django.contrib.auth.models import User
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from snippets.serializers import SnippetSerializer, UserSerializer
 from rest_framework import mixins, generics
+from rest_framework import permissions
+from snippets.permissions import IsOwnerOrReadOnly
 
 
 # @api_view(['GET', 'POST'])
@@ -82,6 +85,10 @@ from rest_framework import mixins, generics
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 # @api_view(['GET', 'PUT', 'DELETE'])
@@ -161,3 +168,12 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
