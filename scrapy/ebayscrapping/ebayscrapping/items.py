42f1f99c3b166ae807e69c0ed3bb2585d5f2fe67
najamulhassanarbi <najam.hassan02@arbisoft.com>
2024-07-31 16:18:38 +0500

    Remove ignored files

--- scrapy/ebayscrapping/ebayscrapping/items.py
+++
@@ -1,9 +0,0 @@
-"""
-This module is for nothing
-"""
-
-import scrapy
-
-
-class EbayscrappingItem(scrapy.Item):
-    pass
--- scrapy/ebayscrapping/ebayscrapping/pipelines.py
+++
@@ -1,7 +0,0 @@
-# Define your item pipelines here
-
-
-
-class EbayscrappingPipeline:
-    def process_item(self, item, spider):
-        return item
