a26fcbc64fec512b676437e88663fa724418bfd6
najamulhassanarbi <najam.hassan02@arbisoft.com>
2024-08-13 11:13:14 +0500

    updated models

--- ArbiShop/orders/models.py
+++ ArbiShop/orders/models.py
@@ -2,17 +2,17 @@ from django.db import models
 
 from django.conf import settings
 from django.db import models
-from products.models import Product
 
 
 from django.db import models
 from django.conf import settings
+from products.models
 
 class Order(models.Model):
     STATUS_CHOICES = [
         ('placed', 'Placed'),
-        ('paid', 'Paid'),
         ('processing', 'Processing'),
+        ('paid', 'Paid'),
         ('delivered', 'Delivered'),
         ('cancelled', 'Cancelled'),
     ]
@@ -22,14 +22,14 @@ class Order(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='placed')
 
-    # Stripe-related fields
     stripe_payment_intent_id = models.CharField(max_length=255, blank=True, null=True)
     stripe_charge_id = models.CharField(max_length=255, blank=True, null=True)
     payment_method = models.CharField(max_length=50, blank=True, null=True)
     payment_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     payment_date = models.DateTimeField(blank=True, null=True)
 
-    # Address fields
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+
     address_line_1 = models.CharField(max_length=255)
     address_line_2 = models.CharField(max_length=255, blank=True, null=True)
     city = models.CharField(max_length=100)
@@ -41,7 +41,6 @@ class Order(models.Model):
         return f"Order {self.id} by {self.user.email}"
 
 
-
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
@@ -49,4 +48,4 @@ class OrderItem(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
-        return f"{self.quantity} of {self.product.name} in order {self.order.id}"
+        return f"{self.quantity} of {self.product.name} in order {self.order.id} having price {self.price}"
